cef773fdbd160801c30928a5853d9340bed9e041
mohamedsalahyahyaoui <mohamedsalah.yahyaoui@esprit.tn>
2016-11-26 16:34:53 +0100

    modified test/src/thread.c

--- test/src/thread.c
+++ test/src/thread.c
@@ -1,7 +1,4 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <pthread.h>
-#include <unistd.h>
+
 
 // A normal C function that is executed as a thread when its name
 // is specified in pthread_create()
